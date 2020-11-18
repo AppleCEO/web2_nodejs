ba619320d084679fecc7c042dc9dee93dd59dd71
도미닉 <kiljh@me.com>
2020-11-18 15:26:18 +0900

    JavaScript - Template Literal

---
+++ syntax/template.js
@@ -0,0 +1,10 @@
+var name = 'egoing';
+var letter = 'Dear '+name+'\n\nHello World!'+name;
+// console.log(letter);
+
+var a = '1';
+
+var letter = `Dear ${name}
+
+Hello World!${name}`;
+console.log(letter);
