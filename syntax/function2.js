5c69d7879bdb1e176065ade677a88437e5b876d5
도미닉 <kiljh@me.com>
2020-11-19 16:55:26 +0900

    JavaScript - 함수의 출력

--- syntax/function2.js
+++ syntax/function2.js
@@ -4,7 +4,7 @@ console.log(Math.round(1.5));
 console.log(Math.round(1.4));
 
 function sum(first, second){
-    console.log(first + second);
+    return first + second;
 }
 
-sum(2,4);
+console.log(sum(2,4));
