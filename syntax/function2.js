d3d5a03708647b2caf81092edc953500cfb90d9f
도미닉 <kiljh@me.com>
2020-11-19 16:48:09 +0900

    JavaScript - 함수의 입력

---
+++ syntax/function2.js
@@ -0,0 +1,10 @@
+const { sign } = require("crypto");
+
+console.log(Math.round(1.5));
+console.log(Math.round(1.4));
+
+function sum(first, second){
+    console.log(first + second);
+}
+
+sum(2,4);
