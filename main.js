dcc58fbbe47113b15c45df5572a17de937a39795
도미닉 <kiljh@me.com>
2020-11-19 14:35:33 +0900

    App - Not found 오류 구현

--- main.js
+++ main.js
@@ -4,41 +4,37 @@ var url = require('url');
 
 var app = http.createServer(function(request,response){
     var queryData = url.parse(request.url, true).query;
+    var pathname = url.parse(request.url, true).pathname;
     var title = queryData.id;
-    
-    if(request.url == '/'){
-      title = 'Welcome';
+
+    if(pathname === '/'){
+      fs.readFile(`data/${title}`, 'utf8', function(err, description){
+        var template = `
+      <!doctype html>
+      <html>
+      <head>
+        <title>WEB1 - ${title}</title>
+        <meta charset="utf-8">
+      </head>
+      <body>
+        <h1><a href="/">WEB</a></h1>
+        <ul>
+          <li><a href="/?id=HTML">HTML</a></li>
+          <li><a href="/?id=CSS">CSS</a></li>
+          <li><a href="/?id=JavaScript">JavaScript</a></li>
+        </ul>
+        <h2>${title}</h2>
+        <p>${description}</p>
+      </body>
+      </html>
+      
+      `;
+      response.writeHead(200);
+      response.end(template);
+      });
+    } else {
+      response.writeHead(404);
+      response.end('Not found');
     }
-    if(request.url == '/favicon.ico'){
-        response.writeHead(404);
-        response.end();
-        return;
-    }
-    response.writeHead(200);
-    fs.readFile(`data/${title}`, 'utf8', function(err, data){
-      var description = data;
-      console.log(description);
-      var template = `
-    <!doctype html>
-    <html>
-    <head>
-      <title>WEB1 - ${title}</title>
-      <meta charset="utf-8">
-    </head>
-    <body>
-      <h1><a href="/">WEB</a></h1>
-      <ul>
-        <li><a href="/?id=HTML">HTML</a></li>
-        <li><a href="/?id=CSS">CSS</a></li>
-        <li><a href="/?id=JavaScript">JavaScript</a></li>
-      </ul>
-      <h2>${title}</h2>
-      <p>${description}</p>
-    </body>
-    </html>
-    
-    `;
-    response.end(template);
-    });
 });
 app.listen(80);
