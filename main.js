f54a97eb7f284b10ba4b20f07f5a82654367324c
도미닉 <kiljh@me.com>
2020-11-23 14:20:20 +0900

    App - 글수정 - 수정 링크 생성

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ var fs = require('fs');
 var url = require('url');
 var qs = require('querystring')
 
-function templateHTML(title, list, body){
+function templateHTML(title, list, body, control){
   return `
           <!doctype html>
           <html>
@@ -14,7 +14,7 @@ function templateHTML(title, list, body){
           <body>
             <h1><a href="/">WEB2</a></h1>
             ${list}
-            <a href="/create">create</a>
+            ${control}
             ${body}
           </body>
           </html>
@@ -25,7 +25,7 @@ function templateList(filelist) {
   var list = '<ul>';
   var i = 0;
   while(i<filelist.length){
-    list = list + `<li><a href="\?id=${filelist[i]}">${filelist[i]}</a></li>`;
+    list = list + `<li><a href="/\?id=${filelist[i]}">${filelist[i]}</a></li>`;
     i = i + 1;
   }
   list = list + '</ul>';
@@ -44,7 +44,7 @@ var app = http.createServer(function(request,response){
           var description = "Hello, Node.js";
           var list = templateList(filelist);
       
-        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a>`);
         response.writeHead(200);
         response.end(template);
         })
@@ -53,7 +53,7 @@ var app = http.createServer(function(request,response){
         fs.readFile(`data/${title}`, 'utf8', function(err, description){
           var list = templateList(filelist);
         
-        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`);
+        var template = templateHTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`);
           response.writeHead(200);
           response.end(template);
           });
@@ -74,7 +74,7 @@ var app = http.createServer(function(request,response){
         <p>
             <input type="submit">
         </p>
-    </form>`);
+    </form>`, '');
       response.writeHead(200);
       response.end(template);
       })
