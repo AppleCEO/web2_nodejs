36ae57076163a4bf30e6c32be7d13f4258d2cc64
도미닉 <kiljh@me.com>
2020-11-23 15:32:04 +0900

    App - 글수정 - 수정된 내용 저장

--- main.js
+++ main.js
@@ -99,6 +99,7 @@ var app = http.createServer(function(request,response){
         
         var template = templateHTML(title, list, `<form action="/update_process" method="post">
         <input type="hidden" name="id" value="${title}">
+        <p><input type="text" name="title" value="${title}"></p>
         <p>
             <textarea name="description" placeholder="description">${description}</textarea>
         </p>
@@ -110,6 +111,23 @@ var app = http.createServer(function(request,response){
           response.end(template);
           });
         });
+    } else if(pathname === '/update_process') {
+      var body = '';
+      request.on('data', function(data){
+        body = body + data;
+      });
+      request.on('end', function(){
+        var post = qs.parse(body);
+        var id = post.id;
+        var title = post.title;
+        var description = post.description;
+        fs.rename(`data/${id}`, `data/${title}`, function(error) {
+          fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+            response.writeHead(302, {Location: `/?id=${title}`});
+            response.end();
+          })
+        })
+      });
     } else {
       response.writeHead(404);
       response.end('Not found');
