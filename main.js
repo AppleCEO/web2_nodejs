db5f2c57f12222205f8dcb1b32920c63b4805ed1
도미닉 <kiljh@me.com>
2020-11-27 09:57:57 +0900

    출력정보에 대한 보안 완료

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var url = require('url');
 var qs = require('querystring')
 var template = require('./lib/template.js');
 var path = require('path');
+var sanitizeHtml = require('sanitize-html');
 
 var app = http.createServer(function(request,response){
     var queryData = url.parse(request.url, true).query;
@@ -25,11 +26,12 @@ var app = http.createServer(function(request,response){
         fs.readdir('./data', function(error, filelist){
           var filteredId = path.parse(queryData.id).base;
           fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-          var list = template.list(filelist);
-        
-        var html = template.HTML(title, list, `<h2>${title}</h2><p>${description}</p>`, `<a href="/create">create</a> <a href="/update?id=${title}">update</a> <form action="delete_process" method="post"><input type="hidden" name="id" value="${title}"><input type="submit" value="delete"></form>`);
-          response.writeHead(200);
-          response.end(html);
+            var sanitizedTitle = sanitizeHtml(title);
+            var sanitizedDescription = sanitizeHtml(description);
+            var list = template.list(filelist);
+            var html = template.HTML(title, list, `<h2>${sanitizedTitle}</h2><p>${sanitizedDescription}</p>`, `<a href="/create">create</a> <a href="/update?id=${sanitizedTitle}">update</a> <form action="delete_process" method="post"><input type="hidden" name="id" value="${title}"><input type="submit" value="delete"></form>`);
+            response.writeHead(200);
+            response.end(html);
           });
         });
       }
